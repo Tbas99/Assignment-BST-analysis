ee8094d9a007de0f40cfdc65123a7fc4fe79b587
Tobias Sagis <tobiassagis@gmail.com>
2022-03-31 12:28:19 +0200

    BST invariant works

--- Assignment Binary Trees.v
+++ Assignment Binary Trees.v
@@ -3,6 +3,9 @@
   Made by:              Tobias Sagis & Luuk Spierings
 **)
 
+(* Libraries to import *)
+Require Import Lia.
+
 (* Define a tree type *)
 Inductive tree : Set :=
   | leaf : tree
@@ -10,31 +13,19 @@ Inductive tree : Set :=
 
 Check node (node (node leaf 2 leaf) 5 (node leaf 7 leaf)) 10 (node (node leaf 12 leaf) 16 (node leaf 17 leaf)).
 
-(* 
-  Before we define the predicate we need some helper functions in
+(*
+  Before we define the predicate we need some helper function in
   order to establish the validity of the binary search tree properties.
+  With this we take a predicate P and check if it holds on all subtree's
+  by inductively traversing the subtree's.
 *)
-Fixpoint bst_left (N : nat) (T : tree) : Prop :=
+Fixpoint bst_prop (P : nat -> tree -> Prop) (T : tree) : Prop :=
   match T with
-    | leaf => True
-    | node l n r => (bst_left n l) /\ (N > n) /\ (bst_right n r)
+  | leaf => True (* A leaf trivially satisfies the bst property *)
+  | node l n r => P n T /\ bst_prop P l /\ bst_prop P r
   end.
 
-Fixpoint bst_right (N : nat) (T : tree) : Prop :=
-  match T with
-    | leaf => True
-    | node l n r => (N < n) /\ (N = n)
-  end.
-
-Fixpoint bst_prop (N : nat) (T : tree) : Prop :=
-  match T with
-    | leaf => True
-    | node l n r => (bst_left
-  end.
-
-Check greater 4 leaf.
-
-(*  
+(*
     Define the predicate on tree to express that a tree is sorted.
     With this we mean that the tree is actually a sorted binary tree,
     or simply a binary search tree.
@@ -48,5 +39,46 @@ Check greater 4 leaf.
 Fixpoint bst (T : tree) : Prop :=
   match T with
   | leaf => True
-  | node l n r => (bst_left n l) \/ ()  bst_left n l /\ bst l /\ (greater n (bst l)) /\ (smaller_or_equal n (bst r))
+  | node l n r => (bst_prop (fun x _ => x < n) l) /\ (bst_prop (fun x _ => (x > n) \/ (x = n)) r) /\ bst l /\ bst r
   end.
+
+(* Check if bst classification is correct *)
+Section verify_bst_invariant.
+
+Definition invalid_bst_tree : Prop :=
+  bst (node (node leaf 6 leaf) 4 (node leaf 10 leaf)).
+
+Definition valid_bst_tree : Prop :=
+  bst (node (node leaf 4 leaf) 6 (node leaf 10 leaf)).
+
+Theorem is_not_bst_tree : ~invalid_bst_tree.
+Proof.
+unfold invalid_bst_tree.
+simpl.
+lia.
+Qed.
+
+Theorem is_bst_tree : valid_bst_tree.
+unfold valid_bst_tree.
+simpl.
+lia.
+Qed.
+
+End verify_bst_invariant.
+
+(*
+  Define a function that takes a binary search tree and a 
+  natural number and inserts the number in the right place
+  in the tree.
+  So that the insertion does not break the binary search tree
+  property.
+*)
+Fixpoint insert (N : nat) (T : tree ) : tree :=
+  match T with
+  | 
+  end.
+
+
+
+
+
